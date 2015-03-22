c2f0924c9763899ed291a3ea5ef386f5afe9c849
Chuck <konefes@gmail.com>
2015-03-22 17:36:24 -0500

    added main.php and login_view.php

---
+++ healtherecords/application/controllers/main.php
@@ -0,0 +1,12 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller 
+{
+
+	public function index()
+	{
+		$this->load->view('login_view');
+	}
+}
