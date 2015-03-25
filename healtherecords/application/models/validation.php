a6501ffee1a9dd5b9fcca85fea3ddd2877837c0b
Chuck <konefes@gmail.com>
2015-03-25 18:04:13 -0500

    made sure to rename validation

--- healtherecords/application/models/validation.php
+++ healtherecords/application/models/validation.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Main extends CI_Controller 
+class Validation extends CI_Controller 
 {
 	public function reg_validation()
 	{
