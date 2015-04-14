bf33b46f4556edb2513f4168048f4dfd02bcd7cf
mfina94 <matthawkeyefina@gmail.com>
2015-04-14 16:25:27 -0500

    Codeception cant see 'complete appointment!' can see if button generated in view, but is generated by ajax call

--- healtherecords/tests/acceptance/PatientMakeApptCept.php
+++ healtherecords/tests/acceptance/PatientMakeApptCept.php
@@ -9,14 +9,30 @@ $I->click('login_submit');
 $I->see('Patient Information');
 $I->click('Make an Appointment');
 $I->see('Specialization');
-$I->fillField("//input[@id='specialization']", 'Caridologist');
-$I->see('Joe Montana');
-$I->see('Male');
+//$I->selectOption("//input[@id='spec']", 'Caridologist');
+//$I->selectOption('.specialization','Cardiologist');
+$I->submitForm('#spec', array('user' => array(
+     'specialization' => 'Cardiologist',
+     'submitButton' => 'Select'
+))
+);
+$I->see('Name');
 $I->click('Select');
 $I->see('Date');
-$I->selectOption('date', '2015-04-15');
-$I->see('Availability for Wednesday');
-$I->selectOption('hours', '5:00 pm');
+$I->submitForm('#datepicker', array('user' => array(
+		'appointment' => '2015-04-15',
+))
+);
+//$I->selectOption('date', '2015-04-15');
+$I->see('Availability');
+//$I->selectOption('hours', '5:00 pm');
+$I->submitForm('#datepicker', array('user' => array(
+		'hours' => '5:00pm',
+		//'date' => '2015-04-15',
+		//'hours' => 'm',
+		'submitButton' => 'Complete Appointment!'
+))
+);
 $I->click('Complete Appointment!');
 $I->see('Your appointment has been made!');
 $I->see('15-4-15 at 5 pm with Dr. Montana');
