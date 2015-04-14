2d674789fb99dc0b82e60b40b86935bf9fdad9a6
mfina94 <matthawkeyefina@gmail.com>
2015-04-14 00:34:50 -0500

    modifying test code. need to reorganize logic

--- healtherecords/tests/acceptance/WelcomeCept.php
+++ healtherecords/tests/acceptance/WelcomeCept.php
@@ -3,8 +3,12 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('perform actions and see result');
 $I->amOnPage('/');
 $I->see('login');
-$I->fillField('username', 'fdsa');
+$I->fillField('username', 'patient1');
 $I->fillField('password', 'asdf');
 $I->click('Login');
 $I->see('Patient Information');
+$I->click('View Medical Record');
+$I->see('Religion: Packer');
+$I->see('2x League MVP');
+$I->see('Weight: 190');
 ?>
