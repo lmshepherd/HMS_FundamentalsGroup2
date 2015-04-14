543246696e416d94d436ae3ad780af0ea52a156b
lshepherd615@gmail.com <lshepherd615@gmail.com>
2015-04-13 21:35:50 -0500

    where did the sign up button go?

--- healtherecords/application/views/login_view.php
+++ healtherecords/application/views/login_view.php
@@ -50,6 +50,15 @@
 					'style'       => 'width:50%',
 					'class' => 'btn-success',
 			);
+			$signup = array(
+					'name'        => 'signup_submit',
+					'id'          => 'signup_submit',
+					'value'       => 'Sign up',
+					'maxlength'   => '100',
+					'size'        => '50',
+					'style'       => 'width:50%',
+					'class' => 'btn-lrg',
+			);
 			
 			echo form_open('main/verify_login', $attributes);
 		
@@ -78,28 +87,6 @@
 			echo form_close();
 			?>
 		
-		
-			<div id="footer-wrapper">
-				<footer id="footer"><p><strong><script type="text/javascript">
-					var date = new Date();
-					var month = new Array(7);
-					month[0] = "January";
-					month[1] = "February";
-					month[2] = "March";
-					month[3] = "April";
-					month[4] = "May";
-					month[5] = "June";
-					month[6] = "July";
-					month[7] = "August";
-					month[8] = "September";
-					month[9] = "October";
-					month[10] = "November";
-					month[11] = "December";
-					var year = date.getYear();
-					if (year < 2000) { year+=1900; }
-					document.write(month[date.getMonth()] + " " + date.getDate() + ", " + year);
-				</script></strong></p></footer>
-			</div>
 		</div>
 	</div>
 
