a94d319052aef9c528856428d443d4e4e4e9545f
lshepherd615@gmail.com <lshepherd615@gmail.com>
2015-04-26 17:35:35 -0500

    coloration

--- healtherecords/application/views/homepage_view.php
+++ healtherecords/application/views/homepage_view.php
@@ -20,7 +20,7 @@ $role = $row->role;
   
   		<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.5.1.min.js"></script>
 		<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.10/jquery-ui.min.js"></script>
-		<link href="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.10/themes/Cupertino/jquery-ui.css" rel="stylesheet" type="text/css" />
+		<link href="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.10/themes/start/jquery-ui.css" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" type="text/css" href="css/Lab8Stylesheet.css">
   
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
